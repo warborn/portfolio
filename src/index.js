2b220acd3d623956b6c900a08ebcf11b284da1fc
= <xxthecreatorxx_live@hotmail.com>
2017-08-12 23:05:31 -0500

    Configure webpack to use sass/scss. Start about-me section. Refactor css using rscss components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import 'particles.js';
-import './index.css';
 
 particlesJS.load('particles-js', 'src/particles.json', function() {
   console.log('callback - particles.js config loaded');
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,17 +1,20 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import webpack from 'webpack';
 
 const htmlWebpackPluginConfig = new HtmlWebpackPlugin({ template: './src/index.html' });
 
 const PATHS = {
   app: path.join(__dirname, 'src'),
-  build: path.join(__dirname, 'dist')
+  build: path.join(__dirname, 'dist'),
+  styles: path.join(__dirname, 'src', 'scss', 'index.scss')
 };
 
 const config = {
   entry: [
-    PATHS.app
+    PATHS.app,
+    PATHS.styles
   ],
   output: {
     path: PATHS.build,
@@ -20,8 +23,21 @@ const config = {
   },
   module: {
     loaders: [
+      {
+        enforce: 'pre',
+        test: /\.scss/,
+        loader: 'import-glob-loader'
+      },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.css$/, exclude: /node_modules/, loader: 'style-loader!css-loader' }
+      { test: /\.css$/, 
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader?importLoaders=1'
+        })
+      },
+      { test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
     ]
   },
   devServer: {
@@ -30,7 +46,13 @@ const config = {
   resolve: {
     modules: [path.resolve('./src'), 'node_modules']
   },
-  plugins: [htmlWebpackPluginConfig]
+  plugins: [
+    htmlWebpackPluginConfig,
+    new ExtractTextPlugin({
+      filename: 'dist/index_bundle.css',
+      allChunks: true
+    })
+  ]
 }
 
 export default config;
