dc7f38a2e97dd9dd14b9a1458fcf74383f21befd
= <xxthecreatorxx_live@hotmail.com>
2017-08-17 19:14:34 -0500

    Change particles.js source and use my own forked version. Import particles JSON config and pass it as an object instead of an URL

--- src/js/particles.js
+++ src/js/particles.js
@@ -1,5 +1,4 @@
-import 'particles.js';
+import 'particles.js/particles';
+import particlesConfig from 'assets/particles.json';
 
-particlesJS.load('particles-js', 'src/assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+particlesJS.load('particles-js', particlesConfig);
